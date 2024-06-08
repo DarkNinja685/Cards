afc100778d2df14bded90db2e10e577c935b9386
DarkNinja685 <151549351+DarkNinja685@users.noreply.github.com>
2024-06-08 09:38:01 +0530

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+
+    return (
+        <div className="Card">
+            <img className="card-image" src="https://pxbar.com/wp-content/uploads/2023/10/itachi-wallpaper-hd-1.jpg" alt="profile pic" height="400px" width="400px"></img>
+            <h2 className="card-title">Itachi Uchiha</h2>
+            <p className="card-read">I watch Naruto,the most enjoyable show.</p>
+        </div>
+    );
+}
+export default Card
